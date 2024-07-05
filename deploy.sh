4a3f7ada49ed19762a8e8ce5684c484e027476d8
t-mbindra <t-mbindra@microsoft.com>
2024-07-05 15:06:23 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ az login --use-device-code
 
 # Deploy ARM template using Azure CLI
 az deployment group create \
-  --resource-group mehak-personal-test \
+  --resource-group <resource-group-name> \
   --template-file ./infra/azure.ai.resources.json
