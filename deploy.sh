8aac4f4cd05eba014e7135badf9d29937c1f02c9
t-mbindra <t-mbindra@microsoft.com>
2024-07-05 18:26:56 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 # Azure CLI login
 az login --use-device-code
 
+az group create -l westus -n ChatWithYourData
 
 # Deploy ARM template using Azure CLI
 az deployment group create \
-  --resource-group <resource-group-name> \
+  --resource-group ChatWithYourData \
   --template-file ./infra/azure.ai.resources.json
